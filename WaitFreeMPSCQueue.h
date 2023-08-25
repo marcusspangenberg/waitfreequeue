08472fbb17ffc77dd0f3ca85127c5b517835dffe
Marcus Spangenberg <marcus.spangenberg@gmail.com>
2023-08-25 14:01:05 +0200

    initial import

---
+++ WaitFreeMPSCQueue.h
@@ -0,0 +1,89 @@
+#pragma once
+
+#include <atomic>
+#include <cassert>
+#include <csignal>
+#include <cstddef>
+#include <cstdlib>
+#include <cstring>
+#include <type_traits>
+#include <unistd.h>
+#include <utility>
+
+template<typename T, size_t S>
+class WaitFreeMPSCQueue
+{
+public:
+    WaitFreeMPSCQueue()
+        : head_(0),
+          tail_(0)
+    {
+        static_assert(isPowerOfTwo(S));
+        const auto pageSize = getpagesize();
+        const auto allocSize = nextMultipleOf(pageSize, sizeof(element) * S);
+        auto allocResult = aligned_alloc(pageSize, allocSize);
+        assert(allocResult);
+        memset(allocResult, 0, allocSize);
+        elements_ = reinterpret_cast<element*>(allocResult);
+    }
+
+    ~WaitFreeMPSCQueue()
+    {
+        free(elements_);
+    }
+
+    template<typename U>
+    void push(U&& item) noexcept
+    {
+        const auto tail = tail_.fetch_add(1, std::memory_order_relaxed) & modValue_;
+        elements_[tail].value_ = std::forward<U>(item);
+        assert(elements_[tail].isUsed_.load(std::memory_order_acquire) == 0);
+        elements_[tail].isUsed_.store(1, std::memory_order_release);
+    }
+
+    bool pop(T& item) noexcept
+    {
+        const auto head = head_.fetch_add(1, std::memory_order_relaxed) & modValue_;
+        if (elements_[head].isUsed_.load(std::memory_order_acquire) == 0)
+        {
+            head_.fetch_sub(1, std::memory_order_relaxed);
+            return false;
+        }
+
+        if constexpr (std::is_move_assignable_v<T>)
+        {
+            item = std::move(elements_[head].value_);
+        }
+        else
+        {
+            item = elements_[head].value_;
+        }
+
+        elements_[head].isUsed_.store(0, std::memory_order_relaxed);
+        return true;
+    }
+
+private:
+    static constexpr size_t cacheLineSize_ = 64;
+    static constexpr uint32_t modValue_ = S - 1;
+
+    struct element {
+        alignas(cacheLineSize_) T value_;
+        std::atomic<uint_fast32_t> isUsed_;
+    };
+
+    alignas(cacheLineSize_) element* elements_;
+    alignas(cacheLineSize_) std::atomic<uint_fast32_t> head_;
+    alignas(cacheLineSize_) std::atomic<uint_fast32_t> tail_;
+
+    static constexpr bool isPowerOfTwo(const size_t size)
+    {
+        return (size & (size - 1)) == 0;
+    }
+
+    static constexpr size_t nextMultipleOf(const size_t n, const size_t size)
+    {
+        assert(isPowerOfTwo(n));
+        return (size + n - 1) & ~(n - 1);
+    }
+};
---
+++ test/TestHelpers.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include <atomic>
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <string>
+#include <utility>
+
+class ScopedTimer
+{
+public:
+    explicit ScopedTimer()
+        : start_(std::chrono::high_resolution_clock::now())
+    {
+    }
+
+    [[nodiscard]] double getMs() const
+    {
+        auto elapsed = std::chrono::high_resolution_clock::now() - start_;
+        return std::chrono::duration<double, std::milli>(elapsed).count();
+    }
+
+private:
+    std::chrono::high_resolution_clock::time_point start_;
+    std::chrono::high_resolution_clock::time_point elapsed_;
+};
+
+template<size_t S>
+class ScopedStatsAverage
+{
+public:
+    explicit ScopedStatsAverage(std::string name)
+        : name_(std::move(name))
+    {
+    }
+
+    ~ScopedStatsAverage()
+    {
+        double sum = 0.0;
+        for (const auto value: values_)
+        {
+            sum += value;
+        }
+
+        printf("%s: %f\n", name_.c_str(), sum / S);
+    }
+
+    void push(const double value)
+    {
+        values_[index_] = value;
+        index_ = (index_ + 1) % S;
+    }
+
+private:
+    size_t index_ = 0;
+    std::array<double, S> values_{0.0};
+    std::string name_;
+};
+
+template<size_t N>
+struct SyncBarrier {
+    std::atomic_flag flagsWait_[N] = {ATOMIC_FLAG_INIT};
+    std::atomic_flag flagsContinue_[N] = {ATOMIC_FLAG_INIT};
+
+    SyncBarrier()
+    {
+        for (size_t i = 0; i < N; ++i)
+        {
+            flagsWait_[i].test_and_set(std::memory_order_acquire);
+            flagsContinue_[i].test_and_set(std::memory_order_acquire);
+        }
+    }
+
+    void arrive(const size_t threadId)
+    {
+        flagsWait_[threadId].clear(std::memory_order_release);
+        while (flagsContinue_[threadId].test_and_set(std::memory_order_acquire))
+        {
+        }
+    }
+
+    void run()
+    {
+        for (size_t i = 0; i < N; ++i)
+        {
+            while (flagsWait_[i].test_and_set(std::memory_order_acquire))
+            {
+            }
+        }
+
+        for (size_t i = 0; i < N; ++i)
+        {
+            flagsContinue_[i].clear(std::memory_order_release);
+        }
+    }
+};
